222fd05d33eb83b49906a9b6cea4a4338ccaea3f
Vladimir Kovalchuk <Vladimir6332@gmail.com>
2019-03-18 17:50:53 +0300

    feat: add func findCheckZeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; i < matrix[i].length; j++) {
+      if (matrix[i][j] == 0 ) {
+        findCheckZeros(i,j)
+      }
+    }
+  }
+
+  function findCheckZeros(x,y) {
+    let arrNumerous = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for ( let i = 0; i < 9; i++) {
+      let positionX = arrNumerous.indexOf(matrix[x][i]);
+      let positionY = arrNumerous.indexOf(matrix[i][y]);
+      if  (positionX > -1) arrNumerous.splice(positionX, 1);
+      if  (positionY > -1) arrNumerous.splice(positionY, 1)
+    }
+    for {}
+
+
+  }
 }
